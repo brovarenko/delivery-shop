f0b30284cc1d6823f13c7c94a09af63cb6b55ec9
brovarenko <brovwg@gmail.com>
2024-03-02 21:29:43 +0100

    add shop route

---
+++ app/api/shops/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+import { db } from '@/lib/prismadb';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    const shop = await db.shop.create({
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(shop);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
