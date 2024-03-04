8107276bb29d86656dbffe14bcf79a38542c341b
brovarenko <brovwg@gmail.com>
2024-03-04 20:41:46 +0100

    fix url

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -3,7 +3,7 @@ import { Product } from '@/types';
 const URL = `${process.env.NEXT_PUBLIC_API_URL}`;
 
 const getProduct = async (id: string): Promise<Product[]> => {
-  const res = await fetch(`http://localhost:3000/api/${id}/products/`);
+  const res = await fetch(`${URL}/api/${id}/products/`);
 
   return res.json();
 };
--- actions/get-shops.tsx
+++ actions/get-shops.tsx
@@ -3,7 +3,7 @@ import { type Shop } from '@/types';
 const URL = `${process.env.NEXT_PUBLIC_API_URL}`;
 
 const getShops = async (): Promise<Shop[]> => {
-  const res = await fetch(`http://localhost:3000/api/shops/`);
+  const res = await fetch(`${URL}/api/shops/`);
   return res.json();
 };
 
